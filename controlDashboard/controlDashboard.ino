5f99bea64c81f322f91cb997ff34c7d7e0ba52f8
Salam <56936701+Salam07@users.noreply.github.com>
2021-11-03 19:12:51 -0400

    Update controlDashboard.ino

--- controlDashboard/controlDashboard.ino
+++ controlDashboard/controlDashboard.ino
@@ -1,9 +1,113 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHTesp.h"
+
+DHTesp dht;
+const char* ssid = "EBOX-2051";
+const char* password = "8ce49c24fb";
+
+const char* mqtt_server = "192.168.1.141";
+
+WiFiClient vanieriot;
+PubSubClient client(vanieriot);
+
+// GPIO 5 = D1
+const int LED = 5;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP-8266 IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messagein;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.println((char)message[i]);
+    messagein += (char)message[i];
+  }
+
+  if(topic=="room/light"){
+    if (messagein == "ON") 
+      digitalWrite(LED, HIGH);
+      Serial.println("Light is ON");
+    if (messagein == "OFF") 
+      digitalWrite(LED, HIGH);
+      Serial.println("Light is OFF");
+  }
+}
+
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+ 
+    
+   //  String clientId = "ESP8266Client-";
+   // clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+   // if (client.connect(clientId.c_str())) {
+           if (client.connect("vanieriot")) {
+
+      Serial.println("connected");  
+      client.subscribe("room/light");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  dht.setup(4, DHTesp::DHT11);
+  pinMode(LED, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+  if (!client.connected()) {
+    reconnect();
+  }
+  if(!client.loop())
+    client.connect("vanieriot");
+
+  float temp= dht.getTemperature();
+  float hum= dht.getHumidity();
+    
+    char tempArr [8];
+    dtostrf(temp,6,2,tempArr);
+    char humArr [8];
+    dtostrf(hum,6,2,humArr);
+      
+       client.publish("IoTlab/ESP","Hello IoTlab");
+       client.publish("IoTlab/temperature", tempArr);
+       client.publish("IoTlab/humidity", humArr);
+      //  client.publish("device/alh",hh);
+
+  delay(5000);
+  }
